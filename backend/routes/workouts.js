f7552306c535d76f0d1dff95b5bbadcb01c259aa
Seth Lillard <lillardseth@gmail.com>
2023-01-18 17:43:58 -0500

    fixed

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,34 +1,32 @@
 const express = require('express');
 const {
-    createWorkout,
-    getWorkouts,
-    getWorkout,
-    deleteWorkout,
-    updateWorkout
-} = require('../controllers/workoutController')
-
-const Workout = require('../models/workoutModel');
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout
+} = require('../controllers/workoutController');
+const requireAuth = require('../middleware/requireAuth');
 
 const router = express.Router();
 
-// GET all workouts
+// require auth for all workout routes
+router.use(requireAuth);
 
+// GET all workouts
 router.get('/', getWorkouts);
 
-// GET single workout
-
+//GET a single workout
 router.get('/:id', getWorkout);
 
 // POST a new workout
-
 router.post('/', createWorkout);
 
 // DELETE a workout
-
 router.delete('/:id', deleteWorkout);
 
 // UPDATE a workout
-
 router.patch('/:id', updateWorkout);
 
-module.exports = router
+
+module.exports = router;
